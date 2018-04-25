fe2af68a1ae62720d2d65e44a2d806ff8e39588e
mjanickicrateandbarrel <38705474+mjanickicrateandbarrel@users.noreply.github.com>
2018-04-25 10:42:03 -0500

    asdg
    asg

--- js/intersection.js
+++ js/intersection.js
@@ -1,18 +1,14 @@
-
-
 const sections = document.querySelectorAll('.sub-dub');
-const config = { //rootMargin: "-40%"
+const config = { rootMargin: "0px"
 };
 
 let observer = new IntersectionObserver(function (entries, self) {
   entries.forEach(entry => {
-   /*
-    console.log(entry.isIntersecting + entry.target.id);
     
     if (entry.isIntersecting && entry.target.id !== "sticky-header-top") {
       intersectionHandler(entry); 
     }
-    */
+    
     if (entry.isIntersecting == false && entry.target.id == "sticky-header-top") {
       document.getElementsByClassName("sticky-nav")[0].classList.add('sticky');
     } else if(entry.isIntersecting == true && entry.target.id == "sticky-header-top"){
@@ -26,7 +22,7 @@ let observer = new IntersectionObserver(function (entries, self) {
 sections.forEach(section => {
   observer.observe(section);
 });
-/*
+
 function intersectionHandler(entry) {
   const id = entry.target.id;
   const currentlyActive = document.querySelector('nav .nav-item.active');
@@ -45,13 +41,13 @@ function intersectionHandler(entry) {
   $(".sticky-nav").animate({scrollLeft: activePosition}, 500);
   
 }
-*/
 
 
+/*
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if ((document.body.scrollTop > document.getElementsByClassName("screens")[0].offsetTop) && (document.body.scrollTop < document.getElementsByClassName("screens")[0].offsetTop + document.getElementsByClassName("screens")[0].offsetHeight)) {
         alert("hi");
     }
-}
+}*/
