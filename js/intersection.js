d4f8e455b3f3846512034da4e3035fd46ad9b799
mjanickicrateandbarrel <38705474+mjanickicrateandbarrel@users.noreply.github.com>
2018-04-25 09:38:20 -0500

    rG
    grg

---
+++ js/intersection.js
@@ -0,0 +1,45 @@
+const sections = document.querySelectorAll('.sub-dub');
+const config = { //rootMargin: "-40%"
+};
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+   
+    console.log(entry.isIntersecting + entry.target.id);
+    
+    if (entry.isIntersecting && entry.target.id !== "sticky-header-top") {
+      intersectionHandler(entry); 
+    }
+    
+    if (entry.isIntersecting == false && entry.target.id == "sticky-header-top") {
+      document.getElementsByClassName("sticky-nav")[0].classList.add('sticky');
+    } else if(entry.isIntersecting == true && entry.target.id == "sticky-header-top"){
+      document.getElementsByClassName("sticky-nav")[0].classList.remove('sticky');
+    }
+    
+    
+  });
+}, config);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+  const id = entry.target.id;
+  const currentlyActive = document.querySelector('nav .nav-item.active');
+  const shouldBeActive = document.querySelector('nav .nav-item[data-ref=' + id + ']');
+  
+  if (currentlyActive) {
+      currentlyActive.classList.remove('active');
+  }
+  if (shouldBeActive) {
+      shouldBeActive.classList.add('active');
+  }
+ 
+  
+  //console.log($(shouldBeActive).offset().left - $(shouldBeActive).parent().offset().left);
+  let activePosition = $(shouldBeActive).offset().left - $(shouldBeActive).parent().offset().left;
+  $(".sticky-nav").animate({scrollLeft: activePosition}, 500);
+  
+}
